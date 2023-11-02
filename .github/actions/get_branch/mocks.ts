d09e3a9ba148cd0c41a8551323713d8ae7d9c43c
noah.jablonski <1964654+jablonnc@users.noreply.github.com>
2023-11-02 12:18:02 -0400

    debug

--- .github/actions/get_branch/mocks.ts
+++ .github/actions/get_branch/mocks.ts
@@ -4,5 +4,5 @@ steps should be skipped in the composite action. If these steps were not
 mocked, they would break the test.
 */
 export const E2E_GET_BRANCH_MOCK_STEPS = [
-  { name: 'comment_branch' },
+  { name: 'comment_branch', mockWith: 'echo "ref=test123" >> $GITHUB_OUTPUT' },
 ];
