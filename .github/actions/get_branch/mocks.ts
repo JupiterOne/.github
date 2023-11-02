68c56a26f50f72eb194de97404bc8f9231b47a45
noah.jablonski <1964654+jablonnc@users.noreply.github.com>
2023-11-02 06:20:17 -0400

    debug

---
+++ .github/actions/get_branch/mocks.ts
@@ -0,0 +1,8 @@
+/*
+This file is automatically leveraged when tests are run to determine which
+steps should be skipped in the composite action. If these steps were not
+mocked, they would break the test.
+*/
+export const E2E_GET_BRANCH_MOCK_STEPS = [
+  { name: 'comment_branch' },
+];
