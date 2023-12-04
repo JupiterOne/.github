c7215fd0369fd4f5da53a56ec829f0264cbbd0f0
Terry Yanko <terry.yanko@jupiterone.com>
2023-12-04 10:35:05 -0500

    remove unnecessary tests

--- .github/actions/promote_docker_image/mocks.ts
+++
@@ -1,7 +0,0 @@
-/*
-This file is automatically leveraged when tests are run to determine which
-steps should be skipped in the composite action. If these steps were not
-mocked, they would break the test.
-*/
-export const PROMOTE_DOCKER_IMAGE_RUN_MOCK_STEPS = [
-];
